cbfe482705f7527c41db64b9e27e27df236cf99f
Chase <chambers.chase@gmail.com>
2023-06-07 15:15:14 -0400

    Starting to add inquirer code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const mysql = require('mysql2');
 const viewAllDepartments = 'SELECT * FROM department;';
 const Employee = require('./employee')
+const inquirer = require('inquirer');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -19,15 +20,45 @@ const db = mysql.createConnection(
 
 // Query database
 db.query(viewAllDepartments, function (err, results) {
-  console.log(results);
+ 
 });
 
 const employee1 = new Employee();
-console.log(employee1.getEmployeeInfoQuery());
+
 
 // Returns employee ID
-db.query(employee1.getEmployeeInfoQuery(), function (err, results) {
-  console.log(results);
-  employee1.updateEmployeeInfo(results[0]);
+const employeeViewAll = () => { db.query(employee1.getEmployeeInfoQuery(), function (err, results) {
+  console.table(results);
 });
+};
 
+const databaseViewAll = () => {
+  db.query('SELECT * FROM department;', function (err, results) {
+    console.table(results);
+  });
+};
+
+const showTables = () => {
+inquirer
+  .prompt([
+    {
+      name: "database_election",
+      type: "list",
+      message: "What do you want to do?",
+      choices: ["View All Departments", "View All Roles", "View All Employees", "Nothing"],
+    },
+  ])
+  .then((answer) => {
+    if (answer.database_election === 'View All Departments') {
+        databaseViewAll();
+        
+    } else if (answer.database_election === 'View All Roles') {
+      databaseViewAll();
+    
+    } else if (answer.database_election === 'View All Employees') {
+    } else { console.log('Bye'); }
+    });
+  };
+    
+
+showTables();
